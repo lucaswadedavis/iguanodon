286985b74d325ec67c1255be314edca4709b56cb
lucaswadedavis <lucaswadedavis@gmail.com>
2016-04-06 15:04:02 -0700

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Iguanodon
 
-Iguanodon is a boilerplate Flask app for dealing with app keys.
+Iguanodon is a boilerplate Flask app, mostly a reminder to myself for later when I've forgotten how exactly to serve static assets and make a route.
 
 ### License: MIT
--- server.py
+++ server.py
@@ -3,10 +3,12 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+# the base route which renders a template
 @app.route('/')
 def index ():
     return render_template('index.html')
 
+# this is how to return a simple JSON object
 @app.route('/message')
 def messages():
 	return jsonify({
@@ -14,6 +16,7 @@ def messages():
         'message': 'Official Iguanodon Message'
         })
 
+# routes can be nested, and strings can be grabbed with the <> syntax
 @app.route('/message/<message>')
 def message(message):
     return jsonify({
